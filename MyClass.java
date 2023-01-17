91963dd4547998bfbc51abf09726dab3f1fed47e
LincolnAlexander <lincoln1662@knights.ucf.edu>
2023-01-16 23:16:46 -0500

    2nd push

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,24 @@
-public class MyClass {
+public class MyClass implements Runnable{
+
+    private int threadNum;
+    public MyClass(int threadNum)
+    {
+      this.threadNum = threadNum;
+    }
+    public void run()
+    {
+      System.out.println("Running..." +threadNum);
+    }
 
     public static void main(String[] args) {
       System.out.println("Hello World, Java app");
+      for(int i = 0; i < 8; i++)
+      {
+        MyClass m = new MyClass(i);
+        Thread my = new Thread(m);
+        my.start();
+      }
+      
+      
     }
   }
