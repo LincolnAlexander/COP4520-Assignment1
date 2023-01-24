7658b55587727934345e92c9dce8920b0fe99411
LincolnAlexander <lincoln1662@knights.ucf.edu>
2023-01-24 15:48:52 -0500

    Pushy push

--- MyClass.java
+++ MyClass.java
@@ -6,28 +6,31 @@ public class MyClass implements Runnable{
     private int threadNum;
     private int startNum;
     private int endNum;
-    public MyClass(int threadNum, int startNum, int endNum)
+    private long sumOfPrimes;
+    private long primesFound;
+    private ArrayList <Integer> maxList;
+    public MyClass(int threadNum, int startNum, int endNum, long sumOfPrimes, long primesFound)
     {
       this.threadNum = threadNum;
       this.startNum = startNum;
       this.endNum = endNum;
+      this.sumOfPrimes = sumOfPrimes;
+      this.primesFound = primesFound;
     }
     public void run()
     {
-      System.out.println("Running..." +threadNum);
-      findPrimes(startNum, endNum);
+      //System.out.println("Running..." +threadNum);
+      findPrimes(startNum, endNum, sumOfPrimes);
     }
 
-    public void findPrimes(int startNum, int endNum)
+    public void findPrimes(int startNum, int endNum, long sumOfPrimes)
     {
       int n = endNum;
-      int number_of_integers = Math.max(startNum, endNum) - Math.min(startNum, endNum) + 1;
       boolean[] primes = new boolean[endNum + 1];
       Arrays.fill(primes, true);
       primes[0] = primes[1] = false;
       
-      // if(startNum == 0)
-      //   startNum = 2;
+     
       for (int i = 2 ; i*i <= n; i++) {
         if (primes[i]) {
             for (int j = i*2; j <= n; j += i) {
@@ -40,114 +43,125 @@ public class MyClass implements Runnable{
     for (int i = startNum; i <= n; i++) {
         if (primes[i]) {
             count++;
+            sumOfPrimes += i;
             //System.out.println(i + " ");
         }
     }
-    System.out.println("Number of prime numbers between 1 and " + endNum + ": " + count);
+
+    if(threadNum == 8)
+    {
+      ArrayList<Integer> l = new ArrayList<>();
+      for(int i = startNum; i <=n; i++)
+      {
+        if(primes[i])
+        {
+          if(l.size() < 10)
+          {
+            l.add(i);
+            continue;
+          }
+          else
+          break;
+        }
+      }
+      this.maxList = l;
+    }
+    this.sumOfPrimes = sumOfPrimes;
+    this.primesFound = count;
+    //System.out.println("Number of prime numbers between "+ startNum +" and " + endNum + "Primes Found: " + count + " sum of primes " + sumOfPrimes);
   }
-    // long start_number = 1;
-    // long end_number = 100000000;
-    // int n = (int)(100000000);
-    // boolean[] primes = new boolean[n+1];
-    // Arrays.fill(primes, true);
-    // primes[0] = false;
-    // int index = 0;
-    // for (long i = 2; i*i <= end_number; i++) {
-    //     if (primes[index]) {
-    //         for (long j = Math.max(i*i, (start_number/i)*i); j <= end_number; j += i) {
-    //             if (j >= start_number){
-    //                 primes[(int)(j - start_number)] = false;
-    //             }
-    //         }
-    //     }
-    //     index++;
-    // }
-    // int count = 0;
-    // for (int i = 0; i <= n; i++) {
-    //     if (primes[i]) {
-    //         count++;
-    //     }
-    // }
-    // System.out.println("Number of prime numbers between " + start_number + " and " + end_number + ": " + count);
-
-    // }
-
+  
     public static void main(String[] args) 
     {
 
       long start = System.currentTimeMillis();
-      int totalPrimesFound = 0;
-      int sumOfPrimes = 0;
+      long end;
+       
+      System.out.println("Hello World, Java app");
+      // Thread 1
+      MyClass m1 = new MyClass(1,1,12500000, 0,0);
+      Thread my1 = new Thread(m1);
+      my1.start();
+      
+      // Thread 2
+      MyClass m2 = new MyClass(2,12500001,25000000,0,0);
+      Thread my2 = new Thread(m2);
+      my2.start();
+
+      // // Thread 3
+      MyClass m3 = new MyClass(3,25000001, 37500000,0,0);
+      Thread my3 = new Thread(m3);
+      my3.start();
+
+      // // Thread 4
+      MyClass m4 = new MyClass(4,37500001, 50000000,0,0);
+      Thread my4 = new Thread(m4);
+      my4.start();
+
+      // // Thread 5
+      MyClass m5 = new MyClass(5, 50000001, 62500000,0,0);
+      Thread my5 = new Thread(m5);
+      my5.start();
+
+      // // Thread 6
+      MyClass m6 = new MyClass(6, 62500001, 75000000,0,0);
+      Thread my6 = new Thread(m6);
+      my6.start();
+
+      // // Thread 7
+      MyClass m7 = new MyClass(7, 75000001, 87500000,0,0);
+      Thread my7 = new Thread(m7);
+      my7.start();
+      
+      // // Thread 8
+      MyClass m8 = new MyClass(8, 87500001, 100000000,0,0);
+      Thread my8 = new Thread(m8);
+      my8.start();
+
+      try
+      {
+        my1.join();
+        my2.join();
+        my3.join();
+        my4.join();
+        my5.join();
+        my6.join();
+        my7.join();
+        my8.join();
+      }
+      catch(Exception e)
+      {
+        System.out.println(e);
+      }
+
+
+      long totalPrimesFound = m1.primesFound + m2.primesFound + m3.primesFound + m4.primesFound + m5.primesFound + m6.primesFound + m7.primesFound + m8.primesFound;
+      long sumOfPrimes = m1.sumOfPrimes + m2.sumOfPrimes + m3.sumOfPrimes + m4.sumOfPrimes + m5.sumOfPrimes + m6.sumOfPrimes + m7.sumOfPrimes + m8.sumOfPrimes;
+      
+      end = System.currentTimeMillis();
+      // System.out.println("Program Finished in: " + (end - start) + "ms");
+      // System.out.println(m8.maxList.toString());
       try
       {
         FileWriter w = new FileWriter("primes.txt");
-        w.write("Execution Time: \n");
-        w.write("Total Number of Primes Found: \n");
-        w.write("Sum of all Primes Found: \n");
-        w.write("Top Ten Maximum Primes: \n");
+        w.write("Execution Time: " + (end - start) + "ms\n");
+        w.write("Total Number of Primes Found: " + totalPrimesFound + "\n");
+        w.write("Sum of all Primes Found: " + sumOfPrimes + "\n");
+        w.write("Top Ten Maximum Primes: " + m8.maxList.toString() + "\n");
         w.close();
       }
       catch(Exception e)
       {
         System.out.println(e);
       }
+
       
-      long start_number = 120000001;
-      long end_number = 25000000;
-      long number_of_integers;
-      if(start_number <= end_number){
-          number_of_integers = end_number - start_number + 1;
-          System.out.println("Number of integers between " + start_number + " and " + end_number + ": " + number_of_integers);
-      }
-      else{
-          number_of_integers = start_number - end_number + 1;
-          System.out.println("Number of integers between " + end_number + " and " + start_number + ": " + number_of_integers);
-      }
       
-      System.out.println("Hello World, Java app" + number_of_integers );
-      // Thread 1
-      MyClass m1 = new MyClass(1,1,12500000);
-      Thread my1 = new Thread(m1);
-      my1.start();
-
-      // Thread 2
-      // MyClass m2 = new MyClass(2,1200000,25000000);
-      // Thread my2 = new Thread(m2);
-      // my2.start();
-
-      // // Thread 3
-      // MyClass m3 = new MyClass(3,25000001, 37500000);
-      // Thread my3 = new Thread(m3);
-      // my3.start();
-
-      // // Thread 4
-      // MyClass m4 = new MyClass(4,37500001, 50000000);
-      // Thread my4 = new Thread(m4);
-      // my4.start();
-
-      // // Thread 5
-      // MyClass m5 = new MyClass(5, 50000001, 62500000);
-      // Thread my5 = new Thread(m5);
-      // my5.start();
-
-      // // Thread 6
-      // MyClass m6 = new MyClass(6, 62500001, 75000000);
-      // Thread my6 = new Thread(m6);
-      // my6.start();
-
-      // // Thread 7
-      // MyClass m7 = new MyClass(7, 75000001, 87500000);
-      // Thread my7 = new Thread(m7);
-      // my7.start();
-
-      // // Thread 8
-      // MyClass m8 = new MyClass(8, 87500001, 100000000);
-      // Thread my8 = new Thread(m8);
-      // my8.start();
-
-
-      long end = System.currentTimeMillis();
-      System.out.println("Program Finished in: " + (end - start) + "ms");
+      
+     
+        
+     
+      
 
       
     }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # COP4520-Assignment1
+
+## Welcome Non-Technical Manager!
+
+To run my program you will need to open your command prompt and download files: MyClass.java
+Navigate to the to the folder where you downloaded that file.
+Compile the file by typing: javac MyClass.java
+Execute the file by typing: java MyClass
+You will then find all neccesary information in primes.txt
+
+Have a good day :)
