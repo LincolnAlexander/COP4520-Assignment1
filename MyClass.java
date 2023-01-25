099058c20fd4bc5277da8daa9fcc827c2890fec3
LincolnAlexander <lincoln1662@knights.ucf.edu>
2023-01-25 11:58:23 -0500

    Fixed max ten primes, added comments, pushy time

--- MyClass.java
+++ MyClass.java
@@ -3,12 +3,15 @@ import java.io.*;
 
 public class MyClass implements Runnable{
 
+    // Variables for thraeds
     private int threadNum;
     private int startNum;
     private int endNum;
     private long sumOfPrimes;
     private long primesFound;
-    private ArrayList <Integer> maxList;
+    private LinkedList <Integer> maxList;
+
+    // Constructor
     public MyClass(int threadNum, int startNum, int endNum, long sumOfPrimes, long primesFound)
     {
       this.threadNum = threadNum;
@@ -17,47 +20,51 @@ public class MyClass implements Runnable{
       this.sumOfPrimes = sumOfPrimes;
       this.primesFound = primesFound;
     }
+    // Runs findPrimes
     public void run()
     {
       //System.out.println("Running..." +threadNum);
       findPrimes(startNum, endNum, sumOfPrimes);
     }
 
+    // Implented Sieve of Eratosthenes
     public void findPrimes(int startNum, int endNum, long sumOfPrimes)
     {
+      
       int n = endNum;
       boolean[] primes = new boolean[endNum + 1];
       Arrays.fill(primes, true);
       primes[0] = primes[1] = false;
       
-     
+     // Loop goes through specific range
       for (int i = 2 ; i*i <= n; i++) {
         if (primes[i]) {
             for (int j = i*2; j <= n; j += i) {
                 primes[j] = false;
             }
-            //System.out.println(i + " ");
+            
         }
     }
+    // Counts all the primes nums for specific range
     int count = 0;
     for (int i = startNum; i <= n; i++) {
         if (primes[i]) {
             count++;
             sumOfPrimes += i;
-            //System.out.println(i + " ");
+          
         }
     }
-
+    // On last thread we gather maximum 10 primes using LinkedList
     if(threadNum == 8)
     {
-      ArrayList<Integer> l = new ArrayList<>();
-      for(int i = startNum; i <=n; i++)
+      LinkedList<Integer> l = new LinkedList<>();
+      for(int i = endNum; i >=startNum; i--)
       {
         if(primes[i])
         {
           if(l.size() < 10)
           {
-            l.add(i);
+            l.addFirst(i);
             continue;
           }
           else
@@ -68,56 +75,63 @@ public class MyClass implements Runnable{
     }
     this.sumOfPrimes = sumOfPrimes;
     this.primesFound = count;
-    //System.out.println("Number of prime numbers between "+ startNum +" and " + endNum + "Primes Found: " + count + " sum of primes " + sumOfPrimes);
+
   }
   
     public static void main(String[] args) 
     {
-
+      // Start Timer
       long start = System.currentTimeMillis();
       long end;
        
       System.out.println("Check primes.txt");
+
+      // Create eight threads and start them.
+
       // Thread 1
       MyClass m1 = new MyClass(1,1,12500000, 0,0);
       Thread my1 = new Thread(m1);
-      my1.start();
       
       // Thread 2
       MyClass m2 = new MyClass(2,12500001,25000000,0,0);
       Thread my2 = new Thread(m2);
-      my2.start();
+      
 
-      // // Thread 3
+      // Thread 3
       MyClass m3 = new MyClass(3,25000001, 37500000,0,0);
       Thread my3 = new Thread(m3);
-      my3.start();
 
-      // // Thread 4
+      // Thread 4
       MyClass m4 = new MyClass(4,37500001, 50000000,0,0);
       Thread my4 = new Thread(m4);
-      my4.start();
 
-      // // Thread 5
+      // Thread 5
       MyClass m5 = new MyClass(5, 50000001, 62500000,0,0);
       Thread my5 = new Thread(m5);
-      my5.start();
-
-      // // Thread 6
+      // Thread 6
       MyClass m6 = new MyClass(6, 62500001, 75000000,0,0);
       Thread my6 = new Thread(m6);
-      my6.start();
 
-      // // Thread 7
+      // Thread 7
       MyClass m7 = new MyClass(7, 75000001, 87500000,0,0);
       Thread my7 = new Thread(m7);
-      my7.start();
       
-      // // Thread 8
+      // Thread 8
       MyClass m8 = new MyClass(8, 87500001, 100000000,0,0);
       Thread my8 = new Thread(m8);
+
+      // Start all threads
+      my1.start();
+      my2.start();
+      my3.start();
+      my4.start();
+      my5.start();
+      my6.start();
+      my7.start();
       my8.start();
 
+
+      // After all threads finish run the rest of code, so we can time how long the program takes.
       try
       {
         my1.join();
@@ -134,13 +148,14 @@ public class MyClass implements Runnable{
         System.out.println(e);
       }
 
-
+      // Get total Primes and sum of Primes from all threads
       long totalPrimesFound = m1.primesFound + m2.primesFound + m3.primesFound + m4.primesFound + m5.primesFound + m6.primesFound + m7.primesFound + m8.primesFound;
       long sumOfPrimes = m1.sumOfPrimes + m2.sumOfPrimes + m3.sumOfPrimes + m4.sumOfPrimes + m5.sumOfPrimes + m6.sumOfPrimes + m7.sumOfPrimes + m8.sumOfPrimes;
       
+      // Stop timer
       end = System.currentTimeMillis();
-      // System.out.println("Program Finished in: " + (end - start) + "ms");
-      // System.out.println(m8.maxList.toString());
+      
+      // Create primes.txt and write neccessary info ot it.
       try
       {
         FileWriter w = new FileWriter("primes.txt");
@@ -155,14 +170,5 @@ public class MyClass implements Runnable{
         System.out.println(e);
       }
 
-      
-      
-      
-     
-        
-     
-      
-
-      
     }
   }
